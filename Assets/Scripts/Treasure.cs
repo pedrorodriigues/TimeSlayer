f4cdd9bc1330c664b36867efce686432d8a750de
Lucas <lucaslagoanogueira@hotmail.com>
2018-07-01 17:32:48 -0300

    Treasure

---
+++ Assets/Scripts/Treasure.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Treasure : MonoBehaviour {
+
+	private bool click = false;
+	public Animator anime;
+
+	void OnTriggerEnter2D(Collider2D other){
+        if (other.gameObject.CompareTag("Player")){
+			anime.SetBool ("click", !click);
+			Application.LoadLevel("ValentinaCroft"); //substituir pela fase que vai depois dessa
+        }
+    }
+	
+	void Update () {
+	}
+	
+	void Start () {
+	}
+}
