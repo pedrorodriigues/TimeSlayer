b42e7ca4e061d21c09efd22c213288d1cf20741a
Andre Gomes <andgomes95@gmail.com>
2018-07-03 13:18:49 -0300

    Valentina phase completed

--- Assets/Scripts/Treasure.cs
+++ Assets/Scripts/Treasure.cs
@@ -5,19 +5,20 @@ using UnityEngine;
 
 public class Treasure : MonoBehaviour {
 
-	private bool click = false;
+	public bool click = false;
 	public Animator anime;
-
 	void OnTriggerEnter2D(Collider2D other){
-        if (other.gameObject.CompareTag("Player")){
+		if (other.gameObject.CompareTag("Player") ){
 			anime.SetBool ("click", !click);
-			Application.LoadLevel("ValentinaCroft"); //substituir pela fase que vai depois dessa
+			StartCoroutine(waitTime());
         }
     }
 	
-	void Update () {
-	}
-	
-	void Start () {
+	public IEnumerator waitTime (){
+		click = false;
+		Debug.Log("HERE");
+		yield return new WaitForSeconds (2);
+		Application.LoadLevel("ValentinaCroft"); //substituir pela fase que vai depois dessa
+
 	}
 }
